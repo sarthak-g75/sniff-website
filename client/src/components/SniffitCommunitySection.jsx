5a11cbb317c8d43b33ee3c7fc9f713c95bd042f5
sarthak783891 <gsarthak783891@gmail.com>
2024-10-27 22:27:20 +0530

    changed vite config

--- client/src/components/SniffitCommunitySection.jsx
+++ client/src/components/SniffitCommunitySection.jsx
@@ -8,15 +8,16 @@ const SniffitCommunitySection = () => {
     <div className='flex flex-col md:flex-row w-full h-full px-48 items-center justify-center py-14'>
       {/* Left Side - Text */}
       <div className='flex flex-col justify-center items-start w-full md:w-1/2 p-8'>
-        <h2 className='text-4xl font-bold mb-4'>
-          Connecting Dog Lovers Everywhere
+        <h2 className='text-6xl  mb-4'>
+          Connecting Dog <br />
+          Lovers Everywhere
         </h2>
-        <p className='text-lg mb-6'>
+        <p className='text-base mb-6'>
           Welcome to Sniffit, your ultimate dog matchmaking app, where
           passionate dog owners find perfect breeding matches, adopt various
           breeds, and share their love for dogs in a vibrant community.
         </p>
-        <button className='px-6 py-3 border border-black text-black rounded-lg font-semibold hover:bg-gray-100'>
+        <button className='px-14 py-3 w-max border border-black text-black rounded-full font-semibold hover:bg-gray-100'>
           Explore
         </button>
       </div>
@@ -30,7 +31,7 @@ const SniffitCommunitySection = () => {
         />
 
         {/* Orange Box Overlay */}
-        <div className='absolute bottom-4 flex left-4 bg-[#d87738] items-center justify-evenly  text-white px-4 py-4 rounded-md text-center w-1/2 h-1/4 gap-6'>
+        <div className='absolute bottom-4 flex left-4 bg-[#d87738] items-center justify-evenly    text-white px-4 py-20 rounded-sm text-center w-2/3 h-1/4 gap-6'>
           <div className='flex flex-col items-start justify-between h-[60px] gap-1'>
             <div className='text-4xl font-bold'>150+</div>
             <div>Join Us</div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Set your desired port number here
+    host: true, // Set to true to allow external access to the server
+  },
 })
