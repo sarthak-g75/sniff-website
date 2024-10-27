8d16e2beea6f6290ccbf2a90c1272ffa680273c5
sarthak783891 <gsarthak783891@gmail.com>
2024-10-27 23:20:49 +0530

    landing made responsove

--- client/src/components/SniffitCommunitySection.jsx
+++ client/src/components/SniffitCommunitySection.jsx
@@ -5,7 +5,7 @@ const imgUrl =
 
 const SniffitCommunitySection = () => {
   return (
-    <div className='flex flex-col md:flex-row w-full h-full px-48 items-center justify-center py-14'>
+    <div className='flex flex-col md:flex-row w-full h-full lg:px-48 items-center justify-center py-14'>
       {/* Left Side - Text */}
       <div className='flex flex-col justify-center items-start w-full md:w-1/2 p-8'>
         <h2 className='text-6xl  mb-4'>
@@ -31,7 +31,7 @@ const SniffitCommunitySection = () => {
         />
 
         {/* Orange Box Overlay */}
-        <div className='absolute bottom-4 flex left-4 bg-[#d87738] items-center justify-evenly    text-white px-4 py-20 rounded-sm text-center w-2/3 h-1/4 gap-6'>
+        <div className='absolute bottom-4 flex left-4 bg-[#d87738] items-center justify-evenly text-white px-4 lg:py-20 py-10 rounded-sm text-center w-2/3 h-1/4 gap-6'>
           <div className='flex flex-col items-start justify-between h-[60px] gap-1'>
             <div className='text-4xl font-bold'>150+</div>
             <div>Join Us</div>
